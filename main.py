506022cb59668aff7dcf8394a39c8b2d974d45dd
Ueo ABC, Ai <53726452+AiueoABC@users.noreply.github.com>
2021-05-23 13:23:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np  # install : conda install numpy
-import pyaudio      # install : conda install pyaudio
+import numpy as np
+import pyaudio
 import noteGenerator as ng
 
 SAMPLE_RATE = 44100
@@ -22,14 +22,15 @@ stream = p.open(format=pyaudio.paFloat32,
                 frames_per_buffer=1024,
                 output=True)
 # Play note
-# play(stream, 261.626, 0.3)  # note#60 C4
-# play(stream, 329.628, 0.3)  # note#64 E4
-# play(stream, 391.995, 0.3)  # note#67 G4
-# play(stream, 523.251, 0.6)  # note#72 C5
-play(stream, ng.note2Hz('C4'), 0.3)  # note#60 C4
-play(stream, ng.note2Hz('E4'), 0.3)  # note#64 E4
-play(stream, ng.note2Hz('G4'), 0.3)  # note#67 G4
-play(stream, ng.note2Hz('C5'), 0.6)  # note#72 C5
+play(stream, ng.note2Hz('C4'), 0.3)  # C4
+play(stream, ng.note2Hz('E4'), 0.3)  # E4
+play(stream, ng.note2Hz('G4'), 0.3)  # G4
+play(stream, ng.note2Hz('C5'), 0.6)  # C5
+
+play(stream, ng.note2Hz('C5'), 0.6)  # C5
+play(stream, ng.note2Hz('G4'), 0.3)  # G4
+play(stream, ng.note2Hz('E4'), 0.3)  # E4
+play(stream, ng.note2Hz('C4'), 0.3)  # C4
 
 # close stream
 stream.close()
